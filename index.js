8fa4d83b1e90206ceb6cca80eb1e65efbdca4d67
shau8122 <shaukatali28122001@gmail.com>
2024-09-16 14:01:54 +0530

    port changes

--- index.js
+++ index.js
@@ -54,5 +54,5 @@ io.on("connection", (socket) => {
 });
 
 // listen
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
